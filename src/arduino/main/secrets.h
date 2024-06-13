2a2c6eb283a19a2d96d3cacde5408b63ae1ed5c4
DaveAlsina <davealsina@gmail.com>
2024-06-13 15:30:43 -0500

    Update secrets.h
    Default credentials

--- src/arduino/main/secrets.h
+++ src/arduino/main/secrets.h
@@ -1,3 +1,9 @@
-#define SECRET_SSID "MaticasTech"
-#define SECRET_PASS "MaticasTechIot"
+#ifndef SECRETS_H
+#define SECRETS_H
+
 #define TIMEOFFSET -5*3600
+#define AP_SSID "Maticas Tech Module"
+#define AP_PASSWORD "love-maticas-tech"
+#define MDNS_NAME "maticas-tech-module"
+
+#endif
